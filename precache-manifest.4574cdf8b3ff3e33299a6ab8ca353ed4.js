fd1948205bd75d34b05af4adb072594b56954949
alpoliakov <alpoliakov73@gmail.com>
2020-07-10 15:56:09 +0300

    Updates

---
+++ precache-manifest.4574cdf8b3ff3e33299a6ab8ca353ed4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1ab53d91b963985ce0c702a1cd9a649",
+    "url": "/movie-app-react-example/index.html"
+  },
+  {
+    "revision": "2143d88f5b22f1bd2e8c",
+    "url": "/movie-app-react-example/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "0696452069a2b0422019",
+    "url": "/movie-app-react-example/static/css/main.48c27d96.chunk.css"
+  },
+  {
+    "revision": "2143d88f5b22f1bd2e8c",
+    "url": "/movie-app-react-example/static/js/2.2ff41a24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app-react-example/static/js/2.2ff41a24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0696452069a2b0422019",
+    "url": "/movie-app-react-example/static/js/main.f0f1b0b1.chunk.js"
+  },
+  {
+    "revision": "fbbbad5e6aecedc3942d",
+    "url": "/movie-app-react-example/static/js/runtime-main.245ac17a.js"
+  }
+]);
